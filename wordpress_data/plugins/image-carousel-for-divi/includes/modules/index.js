af01c2fc6c1abe184f9507f6fd20f4e429fd806f
edijavi <edilata@msn.com>
2023-07-14 20:40:58 +0200

    test base wp image

--- wordpress_data/plugins/image-carousel-for-divi/includes/modules/index.js
+++
@@ -1,5 +0,0 @@
-import DiviCarouselModule from './DiviCarouselModule/DiviCarouselModule';
-import DiviCarouselItemModule from './DiviCarouselItemModule/DiviCarouselItemModule';
-
-export default [DiviCarouselModule, DiviCarouselItemModule];
-
--- wordpress_data/plugins/image-carousel-for-divi/resources/views/wptools-divi-carousel-images.php
+++
@@ -1,3 +0,0 @@
-<section class='<?php echo $module_classes; ?>' id="<?php echo $this->module_id(false); ?>" data-carousel-props='<?php echo json_encode($this->container['carousel_module_fields']->get_carousel_js_params($props)); ?>'>
-	<?php echo $content ?>
-</section>
